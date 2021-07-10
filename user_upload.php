97c4a70b9c6653d6020f2317825f60d305c50452
martinjose83 <admin@Martins-MBP.hub>
2021-07-11 01:17:44 +1200

    initial commit

---
+++ user_upload.php
@@ -0,0 +1,12 @@
+<?php 
+//connect to MySQL database.
+//create users database table(unique index for email)
+//read users.csv file.
+//Name and Surname convert to titlecase.
+//email convert to lowercase.
+//check for valid emailid
+//insert into MySQL database.
+//error message if emailid is repeated.
+
+
+?>
