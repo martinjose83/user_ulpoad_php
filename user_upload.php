c31d3da1a9b2b6341dcee4e038607672ecf2101d
martinjose83 <martinjose83@live.com>
2021-07-14 23:05:57 +1200

    file read option added

--- user_upload.php
+++ user_upload.php
@@ -42,7 +42,54 @@ $sql = "CREATE TABLE IF NOT EXISTS users (
     echo 'ERROR:'.$e->getMessage();
     die("Failed to create Table users" );
   }
-//read users.csv file.
+//read file users.csv.
+$row = 1;
+try
+{
+  $fileName = "users.csv";
+
+  if ( !file_exists($fileName) ) {
+    throw new Exception('File not found.');
+  }$handle = fopen($fileName, "r");
+  if ( !$handle ) {
+    throw new Exception('File open failed.');
+  }  
+  $file_parts = pathinfo($fileName);
+  if($file_parts['extension']!="csv"){
+    throw new Exception('File found is not a csv file');
+  }  
+
+  while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+    $num = count($data);
+    echo " \n $num fields in line $row: \t\t";
+    $row++;
+    for ($c=0; $c < $num; $c++) {
+        echo $data[$c] . "\t\t";
+    }
+
+}
+  fclose($handle);
+
+  // send success JSON
+
+} catch ( Exception $e ) {
+  echo 'ERROR:'.$e->getMessage();
+  // die("Failed to create Table users" );
+
+} 
+// $row = 1;
+// if (($handle = fopen("users1.csv", "r")) !== FALSE) {
+//     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+//         $num = count($data);
+//         echo " \n $num fields in line $row: \t\t";
+//         $row++;
+//         for ($c=0; $c < $num; $c++) {
+//             echo $data[$c] . "\t\t";
+//         }
+
+//     }
+//     fclose($handle);
+// }
 //Name and Surname convert to titlecase.
 //email convert to lowercase.
 //check for valid emailid
